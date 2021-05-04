f075e3f81a5dc5726e17d10e3157bafa5a557c6a
Jonathan Henrique <jonathan1_henrique@hotmail.com>
2021-05-04 20:41:46 -0300

    realizando o two way data binding

--- aprendizado/src/app/app.module.ts
+++ aprendizado/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataBindingComponent } from './compoments/databinding/data-binding/data-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataBindingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
