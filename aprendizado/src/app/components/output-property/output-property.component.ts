2c47df7957ce94cc57031b6a1ffacbcad1edd77d
Jonathan Henrique <jonathan1_henrique@hotmail.com>
2021-05-05 20:38:34 -0300

    aula 16 acesso a DOM e ao Template com ViewChild

--- aprendizado/src/app/components/output-property/output-property.component.ts
+++ aprendizado/src/app/components/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -13,15 +13,17 @@ export class OutputPropertyComponent implements OnInit {
 
   @Output() mudouValor = new EventEmitter();
 
+  @ViewChild('campoInput') campoValorInput: any;
+
   ngOnInit(): void {
   }
 
   decrementa(){
-    this.valor--
+    this.campoValorInput.nativeElement.value--
     this.mudouValor.emit({novoValor: this.valor});
   }
   incrementa(){
-    this.valor++
+    this.campoValorInput.nativeElement.value++
     this.mudouValor.emit({novoValor: this.valor});
   }
 
