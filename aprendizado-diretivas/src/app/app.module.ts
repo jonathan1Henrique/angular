1ad84ab475e5a5e550b0fe5d0a5cfc3849ad0488
Jonathan Henrique <jonathan1_henrique@hotmail.com>
2021-05-06 21:00:17 -0300

    aulas de diretivas  ngSwitch, ngSwitchCase e ngSwitchDefault

--- aprendizado-diretivas/src/app/app.module.ts
+++ aprendizado-diretivas/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SwitchCaseComponent } from './components/switch-case/switch-case.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SwitchCaseComponent
   ],
   imports: [
     BrowserModule,
---
+++ aprendizado-diretivas/src/app/components/switch-case/switch-case.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-switch-case',
+  templateUrl: './switch-case.component.html',
+  styleUrls: ['./switch-case.component.css']
+})
+export class SwitchCaseComponent implements OnInit {
+
+  aba: string = 'Home';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
